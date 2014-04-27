7d8af14352b25fcc88ad20b781e7c10bc0d5d020
zhangyaochun <zhangyaochun@wandoujia.com>
2014-04-27 18:58:29 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+//core lib
+var crypto = require('crypto');
+
+//sign
+var sign = exports.sign = function(val, secret) {
+    //TODO no arguments check
+    //i use createHmac
+    return val + '.' + crypto.createHmac('sha256', secret)
+            .update(val)
+            .digest('base64')
+            .replace(/\=+$/, '');
+   
+};
+
+
+//unsign
+exports.unsign = function(val, secret) {
+    //see sign rule
+    var oldVal = val.slice(0, val.lastIndexOf('.'));
+    var macVal = sign(oldVal, secret);
+
+    //now i use this 
+    if (sign(macVal, secret) == sign(val, secret)) {
+        return oldVal;
+    }
+
+    return false;
+};
